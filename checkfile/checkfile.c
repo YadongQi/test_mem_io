fff020f49a5edc1adf7843c0adfdfa110dc93580
Yadong Qi <yadong.qi@intel.com>
2023-04-23 18:19:10 +0800

    implement bufset/bufcmp to replace memset/memcmp
    Signed-off-by: Yadong Qi <yadong.qi@intel.com>

--- checkfile/checkfile.c
+++ checkfile/checkfile.c
@@ -8,13 +8,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
 
 #define KB *(1024U)
 #define MB *(1024U KB)
 #define GB *(1024U MB)
 
 #define BUFFER_MAXSIZE (1 MB)
+
+static void bufset(void *dest, unsigned char c, size_t size) {
+    __asm__ __volatile__ (
+        "cld        \n\t"
+        "rep stosb  \n\t"
+        :: "D" (dest), "a" (c), "c" (size)
+        );
+}
+
+static int bufcmp(const void *dest, const void *src, size_t size)
+{
+    int ret;
+    __asm__ __volatile__(
+        "xor %%eax, %%eax  \n\t"
+        "cld               \n\t"
+        "repe cmpsb        \n\t"
+        "setnz %%al        \n\t"
+        : "=a"(ret)
+        : "D"((uint64_t)dest), "S"((uint64_t)src), "c"(size)
+    );
+    return ret;
+}
 
 static int check_file(const char *src) {
     FILE *fptr_src = NULL;
@@ -27,10 +48,10 @@ static int check_file(const char *src) {
         return -1;
 
     buf = (unsigned char *)malloc(BUFFER_MAXSIZE);
-    memset(buf, 0, BUFFER_MAXSIZE);
+    bufset(buf, 0, BUFFER_MAXSIZE);
 
     base = (unsigned char *)malloc(BUFFER_MAXSIZE);
-    memset(base, 0xFF, BUFFER_MAXSIZE);
+    bufset(base, 0xFF, BUFFER_MAXSIZE);
 
     // Open destination file for writing
     fptr_src = fopen(src, "r");
@@ -48,7 +69,7 @@ static int check_file(const char *src) {
     int found_corrupt = 0;
     for (int i = 0; i < loop; i++) {
         rd_size = fread(buf, 1, BUFFER_MAXSIZE, fptr_src);
-        if ((memcmp(buf, base, BUFFER_MAXSIZE) != 0) || (rd_size != BUFFER_MAXSIZE)) {
+        if ((bufcmp(buf, base, BUFFER_MAXSIZE) != 0) || (rd_size != BUFFER_MAXSIZE)) {
             printf("Found corrupt data: loop=%d, rd_size=%ld\n", i, rd_size);
             found_corrupt = 1;
             break;
@@ -59,7 +80,7 @@ static int check_file(const char *src) {
 
     if ((file_size%BUFFER_MAXSIZE != 0) && (found_corrupt == 0)) {
         fread(buf, 1, file_size%BUFFER_MAXSIZE, fptr_src);
-        if (memcmp(buf, base, BUFFER_MAXSIZE) != 0) {
+        if (bufcmp(buf, base, BUFFER_MAXSIZE) != 0) {
             printf("Found corrupt data in tail\n");
             found_corrupt = 1;
         }
